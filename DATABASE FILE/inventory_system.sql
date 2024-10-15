39e56856b638f1069d3ecbde9d4b24ddf7693a6e
kishang25 <bantoto.kish25@gmail.com>
2024-10-15 17:09:30 +0800

    new sql file

--- DATABASE FILE/inventory_system.sql
+++ DATABASE FILE/inventory_system.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2021 at 07:57 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Oct 15, 2024 at 11:07 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `inventory_system`
@@ -26,23 +27,30 @@ SET time_zone = "+00:00";
 -- Table structure for table `categories`
 --
 
-CREATE TABLE IF NOT EXISTS `categories` (
-`id` int(11) unsigned NOT NULL,
+CREATE TABLE `categories` (
+  `id` int(11) UNSIGNED NOT NULL,
   `name` varchar(60) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `categories`
 --
 
 INSERT INTO `categories` (`id`, `name`) VALUES
-(1, 'Demo Category'),
-(3, 'Finished Goods'),
-(5, 'Machinery'),
-(4, 'Packing Materials'),
-(2, 'Raw Materials'),
-(8, 'Stationery Items'),
-(6, 'Work in Progress');
+(12, 'Baby Needs'),
+(16, 'Bakery'),
+(15, 'Beer and Wine'),
+(4, 'Beverages'),
+(11, 'Breakfast'),
+(3, 'Canned Goods'),
+(8, 'Cooking and Pantry Needs'),
+(5, 'Dairy'),
+(9, 'Frozen Food'),
+(13, 'Fruits and Vegetables'),
+(6, 'Home Care'),
+(1, 'Meat'),
+(14, 'Pet Care'),
+(2, 'Snacks');
 
 -- --------------------------------------------------------
 
@@ -50,11 +58,11 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 -- Table structure for table `media`
 --
 
-CREATE TABLE IF NOT EXISTS `media` (
-`id` int(11) unsigned NOT NULL,
+CREATE TABLE `media` (
+  `id` int(11) UNSIGNED NOT NULL,
   `file_name` varchar(255) NOT NULL,
   `file_type` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -62,35 +70,35 @@ CREATE TABLE IF NOT EXISTS `media` (
 -- Table structure for table `products`
 --
 
-CREATE TABLE IF NOT EXISTS `products` (
-`id` int(11) unsigned NOT NULL,
+CREATE TABLE `products` (
+  `id` int(11) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
   `quantity` varchar(50) DEFAULT NULL,
   `buy_price` decimal(25,2) DEFAULT NULL,
   `sale_price` decimal(25,2) NOT NULL,
-  `categorie_id` int(11) unsigned NOT NULL,
-  `media_id` int(11) DEFAULT '0',
+  `categorie_id` int(11) UNSIGNED NOT NULL,
+  `media_id` int(11) DEFAULT 0,
   `date` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`id`, `name`, `quantity`, `buy_price`, `sale_price`, `categorie_id`, `media_id`, `date`) VALUES
-(1, 'Demo Product', '48', '100.00', '500.00', 1, 0, '2021-04-04 16:45:51'),
-(2, 'Box Varieties', '12000', '55.00', '130.00', 4, 0, '2021-04-04 18:44:52'),
-(3, 'Wheat', '69', '2.00', '5.00', 2, 0, '2021-04-04 18:48:53'),
-(4, 'Timber', '1200', '780.00', '1069.00', 2, 0, '2021-04-04 19:03:23'),
-(5, 'W1848 Oscillating Floor Drill Press', '26', '299.00', '494.00', 5, 0, '2021-04-04 19:11:30'),
-(6, 'Portable Band Saw XBP02Z', '42', '280.00', '415.00', 5, 0, '2021-04-04 19:13:35'),
-(7, 'Life Breakfast Cereal-3 Pk', '107', '3.00', '7.00', 3, 0, '2021-04-04 19:15:38'),
-(8, 'Chicken of the Sea Sardines W', '110', '13.00', '20.00', 3, 0, '2021-04-04 19:17:11'),
-(9, 'Disney Woody - Action Figure', '67', '29.00', '55.00', 3, 0, '2021-04-04 19:19:20'),
-(10, 'Hasbro Marvel Legends Series Toys', '106', '219.00', '322.00', 3, 0, '2021-04-04 19:20:28'),
-(11, 'Packing Chips', '78', '21.00', '31.00', 4, 0, '2021-04-04 19:25:22'),
-(12, 'Classic Desktop Tape Dispenser 38', '160', '5.00', '10.00', 8, 0, '2021-04-04 19:48:01'),
-(13, 'Small Bubble Cushioning Wrap', '199', '8.00', '19.00', 4, 0, '2021-04-04 19:49:00');
+(1, 'Cream-O', '30', 6.00, 8.00, 2, 0, '2021-04-04 16:45:51'),
+(2, 'Coke 12oz', '150', 15.00, 22.00, 4, 0, '2021-04-04 18:44:52'),
+(3, 'Johnsons Classic Baby Powder 100G', '60', 75.00, 91.00, 12, 0, '2021-04-04 18:48:53'),
+(4, 'Century Tuna 155G', '50', 33.00, 44.00, 3, 0, '2021-04-04 19:03:23'),
+(5, 'Bear Brand Powdered  Milk 1200G', '10', 390.00, 460.00, 5, 0, '2021-04-04 19:11:30'),
+(6, 'Purefoods Tender Juicy Hotdog Regular 1KG', '20', 180.00, 230.00, 9, 0, '2021-04-04 19:13:35'),
+(7, 'Johnie Walker Black Label 1L', '25', 1000.00, 1500.00, 15, 0, '2021-04-04 19:15:38'),
+(8, 'Glade Air Freshener Floral Protection 320ML', '20', 210.00, 265.00, 6, 0, '2021-04-04 19:17:11'),
+(9, 'Disney Woody - Action Figure', '67', 29.00, 55.00, 3, 0, '2021-04-04 19:19:20'),
+(10, 'Hasbro Marvel Legends Series Toys', '106', 219.00, 322.00, 3, 0, '2021-04-04 19:20:28'),
+(11, 'Packing Chips', '78', 21.00, 31.00, 4, 0, '2021-04-04 19:25:22'),
+(12, 'Classic Desktop Tape Dispenser 38', '160', 5.00, 10.00, 8, 0, '2021-04-04 19:48:01'),
+(13, 'Small Bubble Cushioning Wrap', '199', 8.00, 19.00, 4, 0, '2021-04-04 19:49:00');
 
 -- --------------------------------------------------------
 
@@ -98,27 +106,27 @@ INSERT INTO `products` (`id`, `name`, `quantity`, `buy_price`, `sale_price`, `ca
 -- Table structure for table `sales`
 --
 
-CREATE TABLE IF NOT EXISTS `sales` (
-`id` int(11) unsigned NOT NULL,
-  `product_id` int(11) unsigned NOT NULL,
+CREATE TABLE `sales` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `product_id` int(11) UNSIGNED NOT NULL,
   `qty` int(11) NOT NULL,
   `price` decimal(25,2) NOT NULL,
   `date` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `sales`
 --
 
 INSERT INTO `sales` (`id`, `product_id`, `qty`, `price`, `date`) VALUES
-(1, 1, 2, '1000.00', '2021-04-04'),
-(2, 3, 3, '15.00', '2021-04-04'),
-(3, 10, 6, '1932.00', '2021-04-04'),
-(4, 6, 2, '830.00', '2021-04-04'),
-(5, 12, 5, '50.00', '2021-04-04'),
-(6, 13, 21, '399.00', '2021-04-04'),
-(7, 7, 5, '35.00', '2021-04-04'),
-(8, 9, 2, '110.00', '2021-04-04');
+(1, 1, 2, 1000.00, '2021-04-04'),
+(2, 3, 3, 15.00, '2021-04-04'),
+(3, 10, 6, 1932.00, '2021-04-04'),
+(4, 6, 2, 830.00, '2021-04-04'),
+(5, 12, 5, 50.00, '2021-04-04'),
+(6, 13, 21, 399.00, '2021-04-04'),
+(7, 7, 5, 35.00, '2021-04-04'),
+(8, 9, 2, 110.00, '2021-04-04');
 
 -- --------------------------------------------------------
 
@@ -126,8 +134,8 @@ INSERT INTO `sales` (`id`, `product_id`, `qty`, `price`, `date`) VALUES
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-`id` int(11) unsigned NOT NULL,
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
   `name` varchar(60) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -135,18 +143,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `image` varchar(255) DEFAULT 'no_image.jpg',
   `status` int(1) NOT NULL,
   `last_login` datetime DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `password`, `user_level`, `image`, `status`, `last_login`) VALUES
-(1, 'Harry Denn', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1, 'no_image.png', 1, '2021-04-04 19:45:52'),
-(2, 'John Walker', 'special', 'ba36b97a41e7faf742ab09bf88405ac04f99599a', 2, 'no_image.png', 1, '2021-04-04 19:53:26'),
-(3, 'Christopher', 'user', '12dea96fec20593566ab75692c9949596833adc9', 3, 'no_image.png', 1, '2021-04-04 19:54:46'),
-(4, 'Natie Williams', 'natie', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 3, 'no_image.png', 1, NULL),
-(5, 'Kevin', 'kevin', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 3, 'no_image.png', 1, '2021-04-04 19:54:29');
+(1, 'Kisha Gindap', 'Cha', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 1, 'no_image.png', 1, '2024-10-15 09:54:44'),
+(2, 'Hanz Buni', 'Soy', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 1, 'no_image.png', 1, '2024-10-14 15:47:38'),
+(3, 'Ayen Alcala', 'Yen', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 1, 'no_image.png', 1, '2024-10-15 09:54:25');
 
 -- --------------------------------------------------------
 
@@ -154,12 +160,12 @@ INSERT INTO `users` (`id`, `name`, `username`, `password`, `user_level`, `image`
 -- Table structure for table `user_groups`
 --
 
-CREATE TABLE IF NOT EXISTS `user_groups` (
-`id` int(11) NOT NULL,
+CREATE TABLE `user_groups` (
+  `id` int(11) NOT NULL,
   `group_name` varchar(150) NOT NULL,
   `group_level` int(11) NOT NULL,
   `group_status` int(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user_groups`
@@ -178,37 +184,45 @@ INSERT INTO `user_groups` (`id`, `group_name`, `group_level`, `group_status`) VA
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indexes for table `media`
 --
 ALTER TABLE `media`
- ADD PRIMARY KEY (`id`), ADD KEY `id` (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`), ADD KEY `categorie_id` (`categorie_id`), ADD KEY `media_id` (`media_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `categorie_id` (`categorie_id`),
+  ADD KEY `media_id` (`media_id`);
 
 --
 -- Indexes for table `sales`
 --
 ALTER TABLE `sales`
- ADD PRIMARY KEY (`id`), ADD KEY `product_id` (`product_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
- ADD PRIMARY KEY (`id`), ADD KEY `user_level` (`user_level`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_level` (`user_level`);
 
 --
 -- Indexes for table `user_groups`
 --
 ALTER TABLE `user_groups`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `group_level` (`group_level`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `group_level` (`group_level`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -218,32 +232,38 @@ ALTER TABLE `user_groups`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
 --
 -- AUTO_INCREMENT for table `media`
 --
 ALTER TABLE `media`
-MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
 --
 -- AUTO_INCREMENT for table `sales`
 --
 ALTER TABLE `sales`
-MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `user_groups`
 --
 ALTER TABLE `user_groups`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- Constraints for dumped tables
 --
@@ -252,19 +272,20 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- Constraints for table `products`
 --
 ALTER TABLE `products`
-ADD CONSTRAINT `FK_products` FOREIGN KEY (`categorie_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_products` FOREIGN KEY (`categorie_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `sales`
 --
 ALTER TABLE `sales`
-ADD CONSTRAINT `SK` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `SK` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-ADD CONSTRAINT `FK_user` FOREIGN KEY (`user_level`) REFERENCES `user_groups` (`group_level`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_user` FOREIGN KEY (`user_level`) REFERENCES `user_groups` (`group_level`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
