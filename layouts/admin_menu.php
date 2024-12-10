fae116f3aebfd60ca870f87256d223158943f836
BokieTheGreat <hanzoy131@gmail.com>
2024-12-10 12:48:19 +0800

    remove sales report

--- layouts/admin_menu.php
+++ layouts/admin_menu.php
@@ -47,15 +47,4 @@
          <li><a href="add_sale.php">Add Sale</a> </li>
      </ul>
   </li>
-  <li>
-    <a href="#" class="submenu-toggle">
-      <i class="glyphicon glyphicon-duplicate"></i>
-       <span>Sales Report</span>
-      </a>
-      <ul class="nav submenu">
-        <li><a href="sales_report.php">Sales by dates </a></li>
-        <li><a href="monthly_sales.php">Monthly sales</a></li>
-        <li><a href="daily_sales.php">Daily sales</a> </li>
-      </ul>
-  </li>
-</ul>
+  
